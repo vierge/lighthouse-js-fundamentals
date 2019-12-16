fda94d2c9658fa14c42630abbbc22099a80a967a
vierge <marancholia@gmail.com>
2019-12-16 16:08:29 +0000

    added while loop exercise to packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,12 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log ("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
-  console.log((i + 1) + ": " + packingList[i]);
-}
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log((i + 1) + ": " + packingList[i]);
+// }
 
-packingList[3];
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
